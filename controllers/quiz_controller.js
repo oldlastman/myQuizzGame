a898f4b2fe412e6b841014a56fee04662882e907
oldlastman <oldlastman@gmail.com>
2015-08-01 13:19:31 +0200

    dado que uso versión de sequelize diferente hay que cambiar el find por findById para que funcione

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,14 +9,14 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
     res.render('quizes/show', { quiz: quiz});
   })
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  models.Quiz.findById(req.params.quizId).then(function(quiz) {
     if (req.query.respuesta === quiz.respuesta) {
       res.render('quizes/answer', 
                  { quiz: quiz, respuesta: 'Correcto' });
