0b3863e2970984d692ba8da2e3607b44b98693a8
oldlastman <oldlastman@gmail.com>
2015-08-01 12:46:29 +0200

    incluida nueva pregunta en el modelo

--- models/models.js
+++ models/models.js
@@ -37,9 +37,12 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
-                 })
+      Quiz.create(
+                  [{ pregunta: 'Capital de Italia', respuesta: 'Roma'},
+                  { pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+
+                  ]
+                    )
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
