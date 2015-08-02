41e4e1ed320dcea22f184a5f29481e840e652f59
oldlastman <oldlastman@gmail.com>
2015-08-02 23:27:29 +0200

    mod del controlador para guardar la pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -52,3 +52,13 @@ exports.new = function(req, res){
     );
   res.render('quizes/new', {quiz:quiz});
 }
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
+};
