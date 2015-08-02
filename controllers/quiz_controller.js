6f39cd7968a54a6b68381890a5b34b1f27489ad2
oldlastman <oldlastman@gmail.com>
2015-08-02 23:21:01 +0200

    includo controlador para introducir nueva pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,3 +44,11 @@ exports.answer = function(req, res) {
   }
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+
+// GET /quizes/new
+exports.new = function(req, res){
+  var quiz = models.Quiz.build(// crea objeto quiz
+      {pregunta:"Pregunta", respuesta:"Respuesta"}
+    );
+  res.render('quizes/new', {quiz:quiz});
+}
