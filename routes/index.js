42badfe09fd9858a1efc24cb9f885121a97fff93
oldlastman <oldlastman@gmail.com>
2015-08-02 12:24:03 +0200

    incluido autoload

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bienvenido a Quiz' });
-});
-/* GET author page. */
-router.get('/author', function(req, res, next) {
-  res.render('author', {  });
+// Página de entrada (home page)
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
