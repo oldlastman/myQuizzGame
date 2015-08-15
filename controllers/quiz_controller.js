86ee5e958224215d93db1f90c38dc3b3187f375b
oldlastman <oldlastman@gmail.com>
2015-08-15 12:34:33 +0200

    debido a la version de sequalize utilizada hay que usar findone en lugar de find

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById({
+  models.Quiz.findOne({
             where: {
                 id: Number(quizId)
             },
